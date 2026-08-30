92ddc832761b2bcb9def577b09b5fd09e386975a
ratoker-jpg <ratoker@gmail.com>
2026-08-30 16:47:38 +0300

    feat: polish development data and system surfaces

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,7 @@ import './styles/fleetDoctrine.css';
 import './styles/commandSystemRoutes.css';
 import './styles/uiParityShellPlanet.css';
 import './styles/uiParityMapsFleetOps.css';
+import './styles/uiParityDevelopmentData.css';
 import { bindFactionRuntimeAssets } from './assets/bindFactionRuntimeAssets';
 import { createGame, updateGamePresentation } from './game/createGame';
 import {
